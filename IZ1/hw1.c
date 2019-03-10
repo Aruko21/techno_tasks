bd8a5ff7236dc9082db897e91beac753bf1d2a0e
Aleksandr Kosenkov <Marveel@yandex.ru>
2019-03-11 02:55:57 +0300

    fixed issues #1

--- IZ1/hw1.c
+++ IZ1/hw1.c
@@ -27,36 +27,101 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <string.h>
-#include <stddef.h>
+#include <stdint.h>
 #include <errno.h>
+#include <stdbool.h>
 #include <math.h>
 
 #define BUF_SIZE 128
-#define REALLOC_COEF 1.5
-
-#define SUCCESS 0
-#define BUF_MEMORY_ALLOC_ERR -5
-#define BUF_MEMORY_REALLOC_ERR -6
-#define INCORRECT_INPUT_ERR -7
+#define REALLOC_COEF 2		// But the coefficient 1.5 may be more efficient in some situations
 
 
-typedef struct Buffer {
+enum return_codes {
+	SUCCESS = 0,
+	BUF_MEMORY_ALLOC_ERR,
+	BUF_MEMORY_REALLOC_ERR,
+	INCORRECT_INPUT_ERR,
+	INVALID_POINTER_ERR,
+	WRONG_SIZE_CUTTING_ERR,
+	REALLOC_CUTTING_ERR
+};
+
+typedef struct buffer_t {
 	size_t curr_size;		// total size (in bytes)
 	size_t size_elem;		// size of single element
 	void *data;
-} Buffer;
+} buffer_t;
 
-Buffer * allocate_buffer(size_t element_size) {
-	Buffer *user_buf = (Buffer *)malloc(sizeof(Buffer));
+buffer_t * allocate_buffer(size_t element_size);
+int reallocate_buffer(buffer_t *user_buf);
+int cut_buf(buffer_t *user_buf, size_t count);
+void free_buffer(buffer_t *user_buf);
+char * reading_from_stdin(buffer_t *user_buf);
+int digit_input(int64_t *digit);
+int64_t * factorization(int64_t digit);
+bool is_digit_or_delimitor(const char symbol);
+int print_array(int64_t *array);
+void print_err(FILE *stream, int code_of_error, const char *func_name);
+
+
+int main() {
+	int64_t digit = 0;
+	int status = digit_input(&digit);
+	if (status != SUCCESS) {
+		print_err(stderr, status, __func__);
+		fprintf(stdout, "[error]\n");
+		return 0;
+	}
+	int64_t *result = factorization(digit);
+	if (!result) {
+		fprintf(stdout, "[error]\n");
+		return 0;
+	} else {
+		status = print_array(result);
+		if (status != SUCCESS) {
+			print_err(stderr, status, __func__);
+		}
+	}
+	free(result);
+	return 0;
+}
+
+void print_err (FILE *stream, int code_of_error, const char *func_name) {
+	const char *err_msg = NULL;
+	switch (code_of_error) {
+		case BUF_MEMORY_ALLOC_ERR: {
+			err_msg = "Allocating of memory failed";
+			break;
+		}
+		case BUF_MEMORY_REALLOC_ERR: {
+			err_msg = "Reallocating of memory failed";
+			break;
+		}
+		case INCORRECT_INPUT_ERR: {
+			err_msg = "User input is incorrect";
+			break;
+		}
+		case INVALID_POINTER_ERR: {
+			err_msg = "An invalid pointer was passed";
+			break;
+		}
+		default: {
+			err_msg = "Undefined error";
+			break;
+		}
+	}
+	fprintf(stderr, "%s: %s\n", func_name, err_msg);
+	return;
+}
+
+buffer_t * allocate_buffer(size_t element_size) {
+	buffer_t *user_buf = (buffer_t *)malloc(sizeof(buffer_t));
 	if (!user_buf) {
-		fprintf(stderr, "Allocating memory for struct failed\n");
 		return NULL;
 	}
 	user_buf->data = malloc(BUF_SIZE);
 	if (!user_buf->data) {
-		fprintf(stderr, "Allocating memory for buffer failed\n");
 		free(user_buf);
 		return NULL;
 	}
@@ -65,16 +130,20 @@ Buffer * allocate_buffer(size_t element_size) {
 	return user_buf;
 }
 
-void free_buffer(Buffer *user_buf) {
-	free(user_buf->data);
-	free(user_buf);
+void free_buffer(buffer_t *user_buf) {
+	if (user_buf) {
+		free(user_buf->data);
+		free(user_buf);
+	}
 	return;
 }
 
-int reallocate_buffer(Buffer *user_buf) {
+int reallocate_buffer(buffer_t *user_buf) {
+	if (!user_buf) {
+		return BUF_MEMORY_REALLOC_ERR;
+	}
 	void *new_buf = realloc(user_buf->data, (size_t)(user_buf->curr_size * REALLOC_COEF));
 	if (!new_buf) {
-		fprintf(stderr, "Reallocating memory for buffer failed\n");
 		return BUF_MEMORY_REALLOC_ERR;
 	}
 	user_buf->data = new_buf;
@@ -82,23 +151,21 @@ int reallocate_buffer(Buffer *user_buf) {
 	return SUCCESS;
 }
 
-int cut_buf(Buffer *user_buf, ptrdiff_t count) {
-	// Buffer compression to real data size
+int cut_buf(buffer_t *user_buf, size_t count) {
+	//Buffer compression to real data size
 	if (count * user_buf->size_elem >= user_buf->curr_size) {
-		fprintf(stderr, "The size of the real data must be less than the buffer size.\n");
-		return BUF_MEMORY_ALLOC_ERR;
+		return WRONG_SIZE_CUTTING_ERR;
 	}
 	void *new_buf = realloc(user_buf->data, user_buf->size_elem * count);
 	if (!new_buf) {
-		fprintf(stderr, "Reallocating while cutting memory for buffer failed\n");
-		return BUF_MEMORY_REALLOC_ERR;
+		return REALLOC_CUTTING_ERR;
 	}
 	user_buf->data = new_buf;
 	user_buf->curr_size = user_buf->size_elem * count;
 	return SUCCESS;
 }
 
-char * reading_from_stdin(Buffer *user_buf) {
+char * reading_from_stdin(buffer_t *user_buf) {
 	size_t chunk_size = user_buf->curr_size;	// Block of new data from stream
 	char *ptr = (char *)user_buf->data;			// Current position on buffer, equailent to the
 												// stream position
@@ -107,31 +174,62 @@ char * reading_from_stdin(Buffer *user_buf) {
 		end = strchr(ptr, '\n');				// Check for ending of input
 		if (!end) {								// Check for End Of File
 			size_t length = strlen(ptr);
-			if (length < (chunk_size - 1)) {
+			if (length < (chunk_size - 1)) {	// "- 1" means excluding '\0'
 				end = ((char *)(user_buf->data)) + length;
 			}
 		}
 		if (!end) {
 			size_t prev_size = user_buf->curr_size - 1;		// excluding '\0'
-			if (reallocate_buffer(user_buf) != SUCCESS) {
-				return NULL;
+			int status = reallocate_buffer(user_buf);
+			if (status != SUCCESS) {
+				print_err(stderr, status, __func__);
+				end = NULL;
+				break;
 			}
-			ptr = user_buf->data + prev_size;				// Update the position in buffer
+			ptr = user_buf->data + prev_size;
 			chunk_size = user_buf->curr_size - prev_size;
 		} else {
 			*end = '\0';
-			if (cut_buf(user_buf, (end + 1) - (char *)user_buf->data) != SUCCESS) {
-				return NULL;
+			int status = cut_buf(user_buf, (end + 1) - (char *)user_buf->data);
+			if (status != SUCCESS) {
+				print_err(stderr, status, __func__);
+				end = NULL;
+				break;
 			}
+			end = user_buf->data + user_buf->curr_size - 1;
+			// "- 1" at the end needed because the string has format:
+			// "smth...[delimitor]\0" and in process delimitor (= \n or EOF) replaced to \0
+
 		}
 	}
-	return user_buf->data + user_buf->curr_size - 1;
+	return end;
 }
 
-int digit_input(long unsigned *digit) {
+bool is_digit_or_delimitor(const char symbol) {
+	if (((symbol != ' ') && (symbol != '\t'))
+		&& ((symbol < '0') || (symbol > '9'))) {
+		return false;
+	} else {
+		return true;
+	}
+}
+
+int print_array(int64_t *array) {
+	if (!array) {
+		return INVALID_POINTER_ERR;
+	}
+	size_t index = 0;
+	while(array[index] != 0) {
+		printf("%lu ", array[index++]);
+	}
+	return SUCCESS;
+}
+
+int digit_input(int64_t *digit) {
 	// Reading str from stdin and checking it for a digit
-	Buffer * buffer = allocate_buffer(sizeof(char));
+	buffer_t * buffer = allocate_buffer(sizeof(char));
 	if (!buffer) {
+		print_err(stderr, BUF_MEMORY_ALLOC_ERR, __func__);
 		return BUF_MEMORY_ALLOC_ERR;
 	}
 
@@ -141,32 +239,29 @@ int digit_input(long unsigned *digit) {
 		return INCORRECT_INPUT_ERR;
 	}
 	// Now we should checking input on correct
-	int check = 0;						// The flag used for the flag below indicates
+	bool check = false;					// The flag used for the flag below indicates
 										// whether a number entry has occurred before
-	int is_digit_before = 0;			// Flag for the state when there is a space after the digit
+	bool is_digit_before = false;		// Flag for the state when there is a space after the digit
 										// Needed to handle the situation with the input of several numbers
 	for (char* tmp = buffer->data; tmp < end; ++tmp) {
-		if (((*tmp != ' ') && (*tmp != '\t'))
-			&& ((*tmp < '0') || (*tmp > '9'))) {
-			// Wrong symbol
-			free_buffer(buffer);
-			return INCORRECT_INPUT_ERR;
+		if (!is_digit_or_delimitor(*tmp)){
+			check = false;
+			break;
 		}
 		if ((*tmp >= '0') && (*tmp <= '9')) {
-			check = 1;
+			check = true;
 			if (is_digit_before) {
 				// If several numbers were entered
-				free_buffer(buffer);
-				return INCORRECT_INPUT_ERR;
+				check = false;
+				break;
 			}
 		} else {
 			if (check && ((*tmp == ' ') || (*tmp == '\t'))) {
-				is_digit_before = 1;
+				is_digit_before = true;
 			}
 		}
 	}
 	if (!check) {
-		// If the string consisted only of spaces
 		free_buffer(buffer);
 		return INCORRECT_INPUT_ERR;
 	}
@@ -185,18 +280,21 @@ int digit_input(long unsigned *digit) {
 	return SUCCESS;
 }
 
-long unsigned * factorization(long unsigned digit) {
+int64_t * factorization(int64_t digit) {
 	// The main logic of program
-	Buffer *values_arr = allocate_buffer(sizeof(long unsigned));
+	buffer_t *values_arr = allocate_buffer(sizeof(int64_t));
 	if (!values_arr) {
+		print_err(stderr, BUF_MEMORY_ALLOC_ERR, __func__);
 		return NULL;
 	}
-	long unsigned *values = values_arr->data;
-	long unsigned prime = 2;
+	int64_t *values = values_arr->data;
+	int64_t prime = 2;
 	values[0] = 1;
 	size_t index = 1;
 	if (digit == 1) {
-		if (cut_buf(values_arr, (ptrdiff_t)(index + 1)) != SUCCESS) {
+		int status = cut_buf(values_arr, (size_t)(index + 1));
+		if (status != SUCCESS) {
+			print_err(stderr, status, __func__);
 			free_buffer(values_arr);
 			return NULL;
 		}
@@ -206,14 +304,16 @@ long unsigned * factorization(long unsigned digit) {
 		return values;
 	}
 	while ((digit != prime) && (prime <= sqrt(digit))) {
-		// sqrt(digit) - from the divisibility check algorithm
+		// We can use the condition (prime <= (digit / 2)) - but it's less efficient
 		if (digit % prime == 0) {
 			if ((index + 2) * values_arr->size_elem >= values_arr->curr_size) {
 				// index + 2: one incrementtion need for the last element and another one
 				// for null-element
-				if (reallocate_buffer(values_arr) != SUCCESS) {
-					free_buffer(values_arr);
-					return NULL;
+				int status = reallocate_buffer(values_arr);
+				if (status != SUCCESS) {
+					print_err(stderr, status, __func__);
+					values = NULL;
+					break;
 				}
 				values = values_arr->data;
 			}
@@ -224,34 +324,19 @@ long unsigned * factorization(long unsigned digit) {
 			prime++;
 		}
 	}
+	if (!values) {
+		free_buffer(values_arr);
+		return NULL;
+	}
 	values[index++] = digit;
 	values[index] = 0;
-	if (cut_buf(values_arr, (ptrdiff_t)(index + 1)) != SUCCESS) {
+	int status = cut_buf(values_arr, (size_t)(index + 1));
+	if (status != SUCCESS) {
+		print_err(stderr, status, __func__);
 		free_buffer(values_arr);
 		return NULL;
 	}
 	values = values_arr->data;		// Update after cutting
 	free(values_arr);				// We need to save the values address
 	return values;
-}
-
-int main() {
-	long unsigned digit = 0;
-	if (digit_input(&digit) != SUCCESS) {
-		fprintf(stdout, "[error]\n");
-		return 0;
-	}
-	long unsigned *result = factorization(digit);
-	if (!result) {
-		fprintf(stdout, "[error]\n");
-		return 0;
-	} else {
-		size_t index = 0;
-		while(result[index] != 0) {
-			printf("%lu ", result[index++]);
-		}
-		putchar('\n');
-	}
-	free(result);
-	return 0;
 }
